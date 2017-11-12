c46ca539511cb07d8583d21df358cd61cdf51c68
Brokenwind <wangkun6536@163.com>
2017-11-12 20:03:36 +0800

    Add some frequently used operations when using matplotlib

--- 01basic/simple_plot.py
+++ 01basic/simple_plot.py
@@ -14,7 +14,7 @@ ax1.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
 ax2.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
 ax3.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
 ax4.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-plt.show()
+#plt.show()
 
 # there is a convenince method plt.subplots, that creates a new figure and returns a Numpy array containing the created subplot objects
 
@@ -24,6 +24,26 @@ for i in range(2):
     for j in range(2):
         axes[i,j].scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
 plt.subplots_adjust(wspace=0,hspace=0)
+#plt.show()
+
+
+# other details 
+
+fig, ax = plt.subplots()
+x = np.arange(0,2*np.pi,0.1)
+y = np.sin(x)
+# set the X axis ploting range
+plt.xlim(0,10)
+# set X axis ticks
+ticks = ax.set_xticks([0,0.5*np.pi,np.pi,1.5*np.pi,2*np.pi])
+# set lables for each ticks
+labels = ax.set_xticklabels(['zero','one','two','three','four'],rotation=30,fontsize='small')
+# set title
+ax.set_title('the test plot')
+# set X axis label
+ax.set_xlabel('time')
+# set legend
+ax.plot(x,y,'k--',label='one')
+ax.plot(x,np.abs(y),'k.',label='two')
+ax.legend(loc='best')
 plt.show()
-
-
