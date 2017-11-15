23120dcc79335e8c27e20ee5e45fa959c3e6004a
Brokenwind <wangkun6536@163.com>
2017-11-15 10:32:04 +0800

    Selecting learning rates if important

--- 02-AdrewNg-ML-2013/ex1/ex2.py
+++ 02-AdrewNg-ML-2013/ex1/ex2.py
@@ -34,12 +34,19 @@ def normalization(data):
     """
     #data = np.mat(data)
     mean = np.mean(data,axis=0)
+    """
     max = np.max(data,axis=0)
     min = np.min(data,axis=0)
     wid = max - min
+    """
+    stdDev = np.sqrt(np.var(data,axis=0))
     for i in np.arange(0,data.shape[0]):
-        data[i] = (data[i] - mean)/wid
+        data[i] = (data[i] - mean)/stdDev
     return data
+
+def normalEqn(x,y):
+    return (x.T * x).I * (x.T * y)
+    
 
 # pridict the the value for the given x, according to the calculated  theta
 def predict(theta,data):
@@ -49,12 +56,33 @@ def predict(theta,data):
     """
     return np.mat([1].extend(data)) * theta
 
+def originalSolve(data):
+    iterations = [10000, 1000, 100, 10]
+    # if the alpha is bigger, for example 0.0001, the cost function will not converge and not find the result
+    alphas = [0.000000001, 0.00000001, 0.0000001, 0.000001]
+    
+    theta = np.mat(np.zeros((data.shape[1],1)))
+    x = np.delete(data,-1,axis=1)
+    y = data[:,-1]
+    mx = np.hstack((np.ones((x.shape[0],1)),np.mat(x)))
+    my = np.mat(y).T
+    fig, ax = plt.subplots(2,2)
+    for i in np.arange(0,4):
+        thet,j = gradientDescent(mx,my,theta,alphas[i],iterations[i])
+        print thet
+        #print normalEqn(mx,my)
+        row = i / 2
+        col = i % 2
+        ax[row,col].plot(np.arange(1,iterations[i]+1),j)
+        ax[row,col].set_title('the alpha is %.10f' % (alphas[i]))
+    plt.grid(True)
+    plt.show()
 
-if __name__ == '__main__':
-    iterations = 1500
-    alpha = 0.05
-    data = np.loadtxt('ex1data2.txt',delimiter=',') 
+def normalizationSovle(data):
+    # features' normalization
     data = normalization(data)
+    iterations = 10000
+    alpha = 0.001
     theta = np.mat(np.zeros((data.shape[1],1)))
     x = np.delete(data,-1,axis=1)
     y = data[:,-1]
@@ -63,15 +91,15 @@ if __name__ == '__main__':
 
     theta,j = gradientDescent(mx,my,theta,alpha,iterations)
     print theta
+    # use normal equation to get theta 
+    print normalEqn(mx,my)
     fig, ax = plt.subplots()
 
-    #ax.plot(np.arange(1,iterations+1),j)
     ax.plot(np.arange(1,iterations+1),j)
     ax.set_title('The the curve between cost value  and iteration times')
-    #print predict(theta,3500)
     plt.show()
 
-
-
-#data = loadData('ex1data1.txt')
-#plotData(data[0],data[1])
+if __name__ == '__main__':
+    data = np.loadtxt('ex1data2.txt',delimiter=',') 
+    originalSolve(data)
+    #normalizationSovle(data)
