d7a519e697659bee38ffadb7563b740a9d286426
Brokenwind <wangkun6536@163.com>
2017-11-19 21:41:33 +0800

    x.txt is generated by ex2data1.mat, and it is a  humman readable ascill file, I use matplotlib to make  picture digit visible

--- 02-AdrewNg-ML-2013/ex3/picTrans.py
+++ 02-AdrewNg-ML-2013/ex3/picTrans.py
@@ -22,14 +22,40 @@ def matrixToImage(data):
 def showImage(data):
     """To show a grayscale picture with it's relative matrix
     """
-    img = matrixToImage(data)
-    img.show()    
     # use matplotlib to show image directly
     plt.imshow(data, cmap=plt.cm.gray, interpolation='nearest')
     plt.show()
+    """
+    img = matrixToImage(data)
+    img.show()    
+    """
 
+def  randomShow():
+    # x.txt stores 5000 training examples of handwritten digits, each digits has 400(20*20) pixels data
+    data = np.loadtxt('x.txt')
+    num = 25
+    row = col = int(np.sqrt(num))
+    fig,ax = plt.subplots(row,col)
+    # random select num digits
+    rnd = np.random.randint(1,5000,num)
+    sels = data[rnd,:]
+    for i in np.arange(0,num):
+        r = i / row
+        c = i % row
+        pic = sels[i,:].reshape((20,20))
+        ax[r,c].imshow(pic, cmap=plt.cm.gray, interpolation='nearest')
+        # do not show any ticks
+        ax[r,c].set_xticks([])
+        ax[r,c].set_yticks([])
+    # adjust the space between pictues to 0
+    plt.subplots_adjust(wspace=0,hspace=0)
+    plt.show()
+    
 if __name__ == '__main__':
+    """
     filename = 'lena.jpg'
     data = imageToMatrix(filename)
     showImage(data)
     #img.save('lena_1.bmp')
+    """
+    randomShow()
