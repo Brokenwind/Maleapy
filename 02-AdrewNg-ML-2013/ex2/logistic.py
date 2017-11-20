bdb94032152be9b7e4148c4c941804d260d59170
Brokenwind <wangkun6536@163.com>
2017-11-20 14:17:10 +0800

    refactor the logistic.py

--- 02-AdrewNg-ML-2013/ex2/logistic.py
+++ 02-AdrewNg-ML-2013/ex2/logistic.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 import numpy as np
+import scipy.optimize as op
 import matplotlib
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+data = np.loadtxt('ex2data1.txt',delimiter=',')
+x = np.mat(np.delete(data,-1,axis=1))
+x = np.hstack((np.ones((x.shape[0],1)),x))
+y = np.mat(data[:,-1]).T
+m,n = x.shape
 
 def sigmoid(z):
     """sigmoid(z)
@@ -11,32 +18,32 @@ def sigmoid(z):
     """
     return 1.0 / (1.0 + np.exp(-z))
 
-"""
-def costFunction(x,y,theta):
-    data = np.hstack((x,y))
-    data = np.array(data.tolist())
-    return costFunction(data,theta)
-"""
-
-def costFunction(x,y,theta):
-    m = x.shape[0]
+def costFunction(theta):
     y1 = np.array(sigmoid(x*theta))
-    #print y1
     y0 = np.array(y)
-    cost = (-1.0/m)*np.sum(y0 * np.log(y1) + (1.0 - y0) * np.log(1.0 - y1))
+    cost = np.sum(y0 * np.log(y1) + (1.0 - y0) * np.log(1.0 - y1))/m
     return cost
 
-def decent(x,y,alpha,theta,iters):
-    m = x.shape[0]
+def costDer(theta):
+    """
+    compute the derivative of cost funtion
+    """
+    h = np.array(sigmoid(x*theta))
+    error = h - y
+    deri = (1.0/m)*(x.T * error)
+    return deri
+
+def decent(alpha,theta,iters):
+    """
+    Use gradient decent method to compute parameters
+    """
     while iters > 0:
-        h = np.array(sigmoid(x*theta))
-        error = h - y
-        deri = (1.0/m)*(x.T * error)
+        deri = costDer(theta)
         theta = theta - alpha*deri
         iters -= 1
     return theta
 
-def plotScatter(ax,data):
+def plotScatter(ax):
     pos = data[data[:,2] == 1]
     neg = data[data[:,2] == 0]
     ax.scatter(pos[:,0],pos[:,1],c='r',marker='+',label='Admitted')
@@ -45,7 +52,7 @@ def plotScatter(ax,data):
     ax.set_ylabel('Exame 2 score')
     ax.legend(loc='best')
 
-def plotBoundary(ax,x,theta):
+def plotBoundary(ax,theta):
     xmax = np.max(x[:,1])
     xmin = np.min(x[:,1])
     theta = np.array(theta.T)[0]
@@ -54,29 +61,26 @@ def plotBoundary(ax,x,theta):
     ax.plot(score1,score2,label='Boundary')
     ax.legend(loc='best')
 
-def predict(x,theta):
-    x = np.hstack(([1],x))
-    x = np.mat(x)
-    print ("the probability is %.3f" % (sigmoid(x*theta)))
-    h = np.sum(sigmoid(x*theta))
+def predict(x0,theta):
+    x0 = np.hstack(([1],x0))
+    x0 = np.mat(x0)
+    print ("the probability is %.3f" % (sigmoid(x0*theta)))
+    h = np.sum(sigmoid(x0*theta))
     if h > 0.5:
         return 1.0
     else:
         return 0.0
     
 if __name__ == '__main__':
-    alpha = 0.03
-    theta = np.mat([0,0,0]).T
-    iters = 50000
+    alpha = 0.009
+    theta = np.mat(np.zeros((n,1)))
+    iters = 100000
     fig, ax = plt.subplots()
-    data = np.loadtxt('ex2data1.txt',delimiter=',')
-    plotScatter(ax,data)
-    x = np.mat(np.delete(data,-1,axis=1))
-    x = np.hstack((np.ones((x.shape[0],1)),x))
-    y = np.mat(data[:,-1]).T
-    theta = decent(x,y,alpha,theta,iters)    
+    plotScatter(ax)
+    theta = decent(alpha,theta,iters)    
     print theta
     print predict([45,85],theta)
-    plotBoundary(ax,x,theta)
+    plotBoundary(ax,theta)
     #print costFunction(x,y,theta)
     plt.show()
+
