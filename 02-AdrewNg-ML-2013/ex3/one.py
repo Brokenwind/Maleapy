08ac91a85ceea8d85747adf64d8e1c0fb1682243
Brokenwind <wangkun6536@163.com>
2017-11-21 13:07:33 +0800

    Add add functions to predict what the digit in the given picture

--- 02-AdrewNg-ML-2013/ex3/one.py
+++ 02-AdrewNg-ML-2013/ex3/one.py
@@ -6,23 +6,32 @@ import matplotlib
 from matplotlib import pyplot as plt
 from ex2.optimlog import *
 
-def predict(x,theta):
+def proba(x,theta):
+    """calculate probability of x  with the calculated theta
+    """
     n = theta.size
-    if x.size > n:
-        print ("x.size > theta.size, failed")
-        return None
+    x = np.reshape(x,(1,np.size(x)))
+    theta = theta.reshape((n,1))
     if x.size +1 == n:
         x = np.hstack(([1],x))
     if x.size != n:
         print ("cannot adjust x.size == theta.size ")
-        return None
-    theta = theta.reshape((n,1))
-    prob = sigmoid(x.dot(theta))[0]
-    print ("the probability is %.3f" % (prob))
-    if prob > 0.5:
-        return 1.0
-    else:
         return 0.0
+    prob = sigmoid(x.dot(theta))[0]
+    return prob
+
+def predict(x,theta):
+    """Return the most possible digit result of the picture data x
+    """
+    m,n = theta.shape
+    max = 0.0
+    res = 0
+    for i in np.arange(1,m):
+        prob = proba(x,theta[i,:])
+        if prob > max:
+            max = prob
+            res = i
+    return res
 
 if __name__ == '__main__':
     # size of given pictures
@@ -37,6 +46,10 @@ if __name__ == '__main__':
     x = np.hstack((np.ones((np.size(x,0),1)),x))
     m, n = x.shape
     y = np.loadtxt('y.txt')
+
+    # all_res will be expanded to a 11 * n(n is 1 plus the number pixels of picture) matrix, which will store all calculated parameters
+    # all_res[0,:] is redundant
+    # all_res[i,:] ( 1 <= i <= 10 ) will store the  calculated parameters of label i
     all_res = np.zeros((1,n))
     for label in np.arange(1,labels+1):
         init_theta = np.zeros(n)
@@ -45,10 +58,8 @@ if __name__ == '__main__':
         yt[y == label] = 1
         res = optimSolve(init_theta,x,yt,reg=True,lamda=lamda)
         res = res.reshape((1,n))
+        # add  classifier parameters of the current label
         all_res = np.vstack((all_res,res))
-    for i in np.arange(0,500):
-        predict(x[i,:],all_res[10,:])
-    #print res
-    
+    print predict(x[100,:],all_res)
     
     
