9eaeec7521086cd8feb6e5de4436c85b31e0f37d
Brokenwind <wangkun6536@163.com>
2017-11-28 09:19:30 +0800

    Add function addOnes to check if first col is ones, and ones at first col if it is not, and add function to calculate the errors on traning and validation set with diffirent lamdas

--- 02-AdrewNg-ML-2013/ex5/leacurve.py
+++ 02-AdrewNg-ML-2013/ex5/leacurve.py
@@ -3,6 +3,19 @@ sys.path.append('..')
 import numpy as np
 from ex1.linear import *
 
+def addOnes(x,m):
+    """
+    Add a col of 1 at first column of x
+    x: the number of rows
+    """
+    n = x.size/m
+    one = np.ones((m,1))
+    x = x.reshape((m,n))
+    judge = np.sum(x[:,0] == one.flatten())
+    if judge != m:
+        x = np.hstack((one,x))
+    return x
+
 def leacurve(x,y,xval,yval,lamda):
     """
     this function implements code to generate the learning curves that will be useful in debugging learning algorithms.
@@ -12,14 +25,8 @@ def leacurve(x,y,xval,yval,lamda):
     """
     m1 = y.size
     m2= yval.size
-
-    if x.size == m1:
-        x = x.reshape((m1,1))
-        x = np.hstack((np.ones((m1,1)),x))
-    if xval.size == m2:
-        xval = xval.reshape((m2,1))
-        xval = np.hstack((np.ones((m2,1)),xval))
-
+    x = addOnes(x,m1)
+    xval = addOnes(xval,m2)
     m,n = x.shape
     errtrain = np.zeros(m)
     errval = np.zeros(m)
@@ -30,6 +37,24 @@ def leacurve(x,y,xval,yval,lamda):
         errval[i-1] = costFunc(theta,xval,yval)
     return errtrain, errval
 
+def errAndLamda(x,y,xval,yval,lamdas):
+    m1 = y.size
+    m2= yval.size
+    x = addOnes(x,m1)
+    xval = addOnes(xval,m2)
+    nlam = lamdas.size
+    errtrain = np.zeros(nlam)
+    errval = np.zeros(nlam)
+    thetaInit = np.zeros(np.size(x,1))
+    i = 0
+    for lam in lamdas:
+        status,theta = optimSolve(thetaInit,x,y,reg=True,lamda=lam)
+        errtrain[i] = costFunc(theta,x,y)
+        errval[i] = costFunc(theta,xval,yval)
+        i += 1
+    return errtrain, errval
+        
+
 """
 if __name__ == '__main__':
     x = np.loadtxt('x.txt')
@@ -37,5 +62,4 @@ if __name__ == '__main__':
     xval = np.loadtxt('xval.txt')
     yval = np.loadtxt('yval.txt')
     errtrain,errval =  leacurve(x,y,xval,yval,0.0)
-    
 """
